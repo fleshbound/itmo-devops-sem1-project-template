c2655fe98cddceb6650a076fc69ca3db9d89dda3
Valeria Avdeykina <117504017+fleshbound@users.noreply.github.com>
2026-01-19 21:11:29 +0300

    Закомментирована установка postgresql в prepare.sh

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,14 +2,11 @@
 
 go mod download
 
-sudo apt-get update
-sudo apt-get install -y postgresql postgresql-contrib
-
-sudo systemctl start postgresql || true
-sudo systemctl enable postgresql || true
-
-# Ожидание запуска PostgreSQL
-sleep 3
+# Раскомментировать при локальном запуске
+# sudo apt-get update
+# sudo apt-get install -y postgresql postgresql-contrib
+# sudo systemctl start postgresql || true
+# sudo systemctl enable postgresql || true
 
 sudo -u postgres psql -f migrations/1_initilize_schema.sql
 sudo -u postgres psql -f migrations/2_alter_tables.sql
