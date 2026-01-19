28541fe241b47b8088507d40bccb483a87dc3d2f
Valeria Avdeykina <117504017+fleshbound@users.noreply.github.com>
2026-01-19 21:07:30 +0300

    Исправлен запуск сервиса postgresql

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -5,7 +5,11 @@ go mod download
 sudo apt-get update
 sudo apt-get install -y postgresql postgresql-contrib
 
-sudo service postgresql start
+sudo systemctl start postgresql || true
+sudo systemctl enable postgresql || true
+
+# Ожидание запуска PostgreSQL
+sleep 3
 
 sudo -u postgres psql -f migrations/1_initilize_schema.sql
 sudo -u postgres psql -f migrations/2_alter_tables.sql
