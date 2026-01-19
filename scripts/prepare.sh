ffb95f6c5fddb06838bd2a20e52249f393cc444c
Valeria Avdeykina <117504017+fleshbound@users.noreply.github.com>
2026-01-19 21:23:26 +0300

    Исправлены миграции

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,12 +2,6 @@
 
 go mod download
 
-# sudo apt-get update
-# sudo apt-get install -y postgresql postgresql-contrib
-# sudo systemctl start postgresql || true
-# sudo systemctl enable postgresql || true
-
 export PGPASSWORD=val1dat0r
-psql -h localhost -p 5432 -U validator -f migrations/1_initilize_schema.sql
-psql -h localhost -p 5432 -U validator -f migrations/2_alter_tables.sql
-# sudo -U validator psql -h localhost -f migrations/3_grant_access.sql
+psql -h localhost -p 5432 -U validator -d project-sem-1 -f migrations/1_initilize_schema.sql
+psql -h localhost -p 5432 -U validator -d project-sem-1 -f migrations/2_alter_tables.sql
